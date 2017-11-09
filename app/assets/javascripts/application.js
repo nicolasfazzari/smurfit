8862ad30e76360c99c3b055b4c4131f4c6888975
nicolasfazzari <nicolasfazzari@gmail.com>
2017-11-09 20:15:31 +0100

    doc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-3.2.1.slim.min
+//= require jquery-3.2.1.min
 //= require jquery-ui
 //= require jquery_ujs
 //= require dataTables.min
