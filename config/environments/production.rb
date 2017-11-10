8a601ddda23c5758ae1dfe60ae080ef70f0211ee
nicolasfazzari <nicolasfazzari@gmail.com>
2017-11-10 22:20:34 +0100

    bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
+  s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
   storage: :s3,
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
