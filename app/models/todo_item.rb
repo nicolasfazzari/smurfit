f43da345554e4afee4aea6776ffd370d5228ead3
nicolasfazzari <nicolasfazzari@gmail.com>
2017-11-18 16:13:41 +0100

    update date

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,7 +2,7 @@ class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
  	acts_as_taggable
 	acts_as_taggable_on :tag_list
-	accepts_nested_attributes_for :todo_list
+	
 	def completed?
 		!completed_at.blank?
 	end
