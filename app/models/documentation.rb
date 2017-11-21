a8e505a1bdb6bd72b21526b07479edfb6afaea58
nicolasfazzari <nicolasfazzari@gmail.com>
2017-11-21 16:49:19 +0100

    xlsx

--- app/models/documentation.rb
+++ app/models/documentation.rb
@@ -1,4 +1,4 @@
 class Documentation < ActiveRecord::Base
 	has_attached_file :file
-	validates_attachment :file, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
+	validates_attachment :file, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/vnd.openxmlformats-officedocument.wordprocessingml.document application/vnd.openxmlformats-officedocument.presentationml.presentation application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)}
 end
