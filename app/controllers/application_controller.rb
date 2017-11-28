a19d3af69ddbcb9c39b95f44c9b7bc399fc4a6df
nicolasfazzari <nicolasfazzari@gmail.com>
2017-11-28 22:18:03 +0100

    progressbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
- 
+ def current_controller?(names)
+  names.include?(params[:controller]) unless params[:controller].blank? || false
+ end
+
+ helper_method :current_controller?
 
 end
